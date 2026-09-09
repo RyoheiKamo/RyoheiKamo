2e3274f826ac46d1e362c03efd5aa9cfd1e9b686
Ryohei Kamo <ryohei.kamo0944@gmail.com>
2026-09-10 07:35:57 +0900

    feat: aggregate ts and tsx as TypeScript

--- scripts/generate-stats.sh
+++ scripts/generate-stats.sh
@@ -1,3 +1,4 @@
+```bash
 #!/bin/bash
 
 set -e
@@ -15,15 +16,63 @@ EOF
 
 awk -F',' '
 BEGIN {
-    allowed["PHP"] = 1
-    allowed["Go"] = 1
-    allowed["Rust"] = 1
-    allowed["TypeScript"] = 1
-    allowed["JavaScript"] = 1
-    allowed["Python"] = 1
+    php_files = php_code = 0
+    go_files = go_code = 0
+    rust_files = rust_code = 0
+    ts_files = ts_code = 0
+    js_files = js_code = 0
+    python_files = python_code = 0
 }
-NR > 1 && allowed[$2] {
-    printf "| %s | %s | %s |\n", $2, $1, $5
+
+NR > 1 {
+    language = $2
+    files = $1
+    code = $5
+
+    if (language == "PHP") {
+        php_files += files
+        php_code += code
+    }
+    else if (language == "Go") {
+        go_files += files
+        go_code += code
+    }
+    else if (language == "Rust") {
+        rust_files += files
+        rust_code += code
+    }
+    else if (language == "TypeScript" || language == "TypeScript JSX") {
+        ts_files += files
+        ts_code += code
+    }
+    else if (language == "JavaScript" || language == "JavaScript JSX") {
+        js_files += files
+        js_code += code
+    }
+    else if (language == "Python") {
+        python_files += files
+        python_code += code
+    }
+}
+
+END {
+    if (php_files > 0)
+        printf "| PHP | %d | %d |\n", php_files, php_code
+
+    if (go_files > 0)
+        printf "| Go | %d | %d |\n", go_files, go_code
+
+    if (rust_files > 0)
+        printf "| Rust | %d | %d |\n", rust_files, rust_code
+
+    if (ts_files > 0)
+        printf "| TypeScript | %d | %d |\n", ts_files, ts_code
+
+    if (js_files > 0)
+        printf "| JavaScript | %d | %d |\n", js_files, js_code
+
+    if (python_files > 0)
+        printf "| Python | %d | %d |\n", python_files, python_code
 }
 ' cloc.csv >> "$OUTPUT"
 
@@ -53,3 +102,4 @@ new_text = (
 
 readme.write_text(new_text)
 PY
+```
